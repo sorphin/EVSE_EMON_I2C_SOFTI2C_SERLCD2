3ea54705c714882f4662324a045ca727fd9b03f0
sorphin <sorphin@Daniels-MBP.lan>
2016-12-01 16:32:27 -0600

    Added Copyright

--- EVSE_EMON_I2C_SOFTI2C_SERLCD2.ino
+++ EVSE_EMON_I2C_SOFTI2C_SERLCD2.ino
@@ -1,3 +1,26 @@
+/*
+ * Open EVSE Energy Monitoring Code
+ * Portions of this are derived from various research online.
+ *
+ * Copyright (c) 2016+ Daniel Benedict <sorphin@gmail.com>
+ * All included libaries are Copyright (c) their respective authors.
+ *
+ * This Software Module is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+
+ * This Software Module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this code; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 //#if defined(ARDUINO) && (ARDUINO >= 100)
 //#include "Arduino.h"
 //#else
